b058789a6388b1b49804ebd218cd7f3b48d1c9bb
NemanjaKostic <unnilstudios@gmail.com>
2016-12-16 15:15:50 +0100

    Add files via upload

---
+++ home/home.controller.js
@@ -0,0 +1,75 @@
+﻿(function () {
+    'use strict';
+
+   
+
+    angular
+        .module('app')
+        .controller('HomeController', HomeController);
+
+    
+    HomeController.$inject = ['UserService', '$rootScope', 'FlashService', '$scope'];
+    function HomeController(UserService, $rootScope, FlashService, $scope) {
+        var vm = this;
+
+        vm.user = null;
+        vm.allUsers = [];
+        vm.deleteUser = deleteUser;
+        vm.register = register;
+
+        initController();
+
+        function register() {
+            vm.dataLoading = true;
+            UserService.Update(vm.user)
+                .then(function (response) {
+                    FlashService.Success('Changed your data successful', true);
+                    
+                        
+                });
+            }
+
+        function register2() {
+            vm.dataLoading = true;
+            vm.user.firstName =  selectedUser.firstName;
+            UserService.Update(vm.user)
+                .then(function (response) {
+                    FlashService.Success('Changed your data successful', true);
+                    
+                        
+                });
+            }    
+
+        function initController() {
+            loadCurrentUser();
+            loadAllUsers();
+        }
+
+        function loadCurrentUser() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+                    vm.user = user;
+                });
+        }
+
+        function loadAllUsers() {
+            UserService.GetAll()
+                .then(function (users) {
+                    vm.allUsers = users;
+                });
+        }
+
+        function deleteUser(id) {
+            UserService.Delete(id)
+            .then(function () {
+                loadAllUsers();
+            });
+        }
+
+        $scope.showMe = false;
+        $scope.updateToggle = function() {
+        $scope.showMe = !$scope.showMe;
+        }
+    }
+
+})();
---
+++ register/register.controller.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('RegisterController', RegisterController);
+
+    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService', '$scope'];
+    function RegisterController(UserService, $location, $rootScope, FlashService, $scope) {
+        var vm = this;
+
+        vm.register = register;
+        vm.registerdate = registerdate;
+
+        function registerdate() {
+            var x = new Date();
+        }
+
+        function register() {
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        $location.path('/login');
+                        registerdate();
+                        
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+    }
+
+})();
